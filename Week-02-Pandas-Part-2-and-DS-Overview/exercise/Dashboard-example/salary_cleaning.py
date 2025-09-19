115d85e9d872e74182c110cf6384a52ec2c065ca
AimeePatience <137180181+AimeePatience@users.noreply.github.com>
2025-09-19 23:51:41 +0000

    ok

--- Week-02-Pandas-Part-2-and-DS-Overview/exercise/Dashboard-example/salary_cleaning.py
+++ Week-02-Pandas-Part-2-and-DS-Overview/exercise/Dashboard-example/salary_cleaning.py
@@ -3,7 +3,6 @@ import pandas as pd
 filepath ='/workspaces/ds-fall-2025-fri-0630/Week-02-Pandas-Part-2-and-DS-Overview/exercise/salary_cleaned.csv'
 def clean_salary(filepath):
     df = pd.read_csv(filepath)
-    
     df = df.dropna(subset=['salary', 'industry', 'title'])
     df['salary'] = pd.to_numeric(df['salary'], errors='coerce')
     df['additional_compensation'] = pd.to_numeric(df['additional_compensation'], errors='coerce')
